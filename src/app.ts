e23e9e9c05755a806e5e4ae0c369f32071d72800
Aline Pons <alinepons@hotmail.com>
2022-03-31 22:00:16 -0300

    commit inicial

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+
+  res.json({server: "pdagro"});
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Servidor rodando em https://localhost:${port}`);
+});
